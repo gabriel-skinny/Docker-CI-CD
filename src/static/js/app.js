d865c232850518bc0b54db65f27cae7e675c86f2
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-30 10:38:35 -0300

    Changing placeholder name

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={(e) => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New good Item"
+                    placeholder="New Item"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
